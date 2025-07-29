d93394c59b318af166576098d20d00dc28353097
klu2300033016 <2300033016@kluniversity.in>
2025-07-29 12:34:59 +0530

    welcome

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      welcome to github pages
+    </>
+  )
+}
+
+export default App
